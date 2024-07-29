71e28ff2d60a935c2693d2c8126011c907b02766
Florian Schneider <schneider.f@me.com>
2024-07-29 21:49:26 +0200

    Add migration API route

--- pages/api/migrate.js
+++ pages/api/migrate.js
@@ -1,9 +1,9 @@
 // pages/api/migrate.js
-import { exec } from 'child_process';
+import { exec } from "child_process";
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    exec('npx prisma migrate deploy', (error, stdout, stderr) => {
+  if (req.method === "POST") {
+    exec("npx prisma migrate deploy", (error, stdout, stderr) => {
       if (error) {
         console.error(`Error: ${error.message}`);
         return res.status(500).json({ error: error.message });
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
       return res.status(200).json({ stdout: stdout });
     });
   } else {
-    res.setHeader('Allow', ['POST']);
+    res.setHeader("Allow", ["POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
