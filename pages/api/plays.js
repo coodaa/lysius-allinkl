31cbb84d0d2823e53ee146b7c74fec90b15e0c08
Florian Schneider <schneider.f@me.com>
2024-07-29 21:46:11 +0200

    migrate

--- pages/api/plays.js
+++ pages/api/plays.js
@@ -2,27 +2,11 @@
 import prisma from "../../lib/prisma";
 
 export default async function handler(req, res) {
-  if (req.method === "GET") {
-    try {
-      const plays = await prisma.play.findMany();
-      res.status(200).json(plays);
-    } catch (error) {
-      res.status(500).json({ error: "Database query error" });
-    }
-  } else if (req.method === "POST") {
-    const { title, description, imageUrl, videoUrl } = req.body;
-    try {
-      const newPlay = await prisma.play.create({
-        data: {
-          title,
-          description,
-          imageUrl,
-          videoUrl,
-        },
-      });
-      res.status(201).json(newPlay);
-    } catch (error) {
-      res.status(500).json({ error: "Database insertion error" });
-    }
+  try {
+    const plays = await prisma.play.findMany();
+    res.status(200).json(plays);
+  } catch (error) {
+    console.error("Error fetching plays:", error);
+    res.status(500).json({ error: "Error fetching plays" });
   }
 }
