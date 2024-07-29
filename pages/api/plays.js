3975967a03e2bdc7ee75d4ec1806ae7c4b4b74b1
Florian Schneider <schneider.f@me.com>
2024-07-29 23:08:31 +0200

    Ensure correct database connection

--- pages/api/plays.js
+++ pages/api/plays.js
@@ -2,16 +2,13 @@ import prisma from "../../lib/prisma";
 
 export default async function handler(req, res) {
   console.log("API handler called"); // Debugging-Ausgabe
-
   try {
-    console.log("Attempting to fetch plays from the database...");
+    console.log("Attempting to fetch plays from the database..."); // Debugging-Ausgabe
     const plays = await prisma.play.findMany();
-    console.log("Plays fetched from database:", plays);
+    console.log("Plays fetched from database:", plays); // Debugging-Ausgabe
     res.status(200).json(plays);
   } catch (error) {
-    console.error("Error fetching plays:", error);
-    res
-      .status(500)
-      .json({ error: "Error fetching plays", details: error.message });
+    console.error("Error fetching plays from database:", error); // Debugging-Ausgabe
+    res.status(500).json({ error: "Error fetching plays" });
   }
 }
